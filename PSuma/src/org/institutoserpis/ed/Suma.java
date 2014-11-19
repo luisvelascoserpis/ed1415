e3640c54c2d4884e6e170504519d7a9095c93d3b
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2014-11-19 08:56:23 +0100

    Añadido PSuma

---
+++ PSuma/src/org/institutoserpis/ed/Suma.java
@@ -0,0 +1,44 @@
+package org.institutoserpis.ed;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Programa para sumar dos números.");
+		
+		
+		System.out.print("Introduce el primero: ");
+		Scanner scanner = new Scanner(System.in);
+		String primerDato = scanner.nextLine();
+		
+		BigDecimal primerNumero = new BigDecimal(primerDato);
+		System.out.println("Primer número=" + primerNumero);
+		
+		System.out.print("Introduce el segundo: ");
+		String segundoDato = scanner.nextLine();
+		
+		BigDecimal segundoNumero = new BigDecimal(segundoDato);
+		System.out.println("Segundo número=" + segundoNumero);
+		
+		BigDecimal suma = primerNumero.add(segundoNumero);
+
+//		Con float tendremos problemas con "el redondeo"		
+//		float primerNumero = Float.parseFloat(primerDato);
+//		System.out.println("Primer número=" + primerNumero);
+//		
+//		System.out.print("Introduce el segundo: ");
+//		String segundoDato = scanner.nextLine();
+//		
+//		float segundoNumero = Float.parseFloat(segundoDato);
+//		System.out.println("Segundo número=" + segundoNumero);
+//		
+//		float suma = primerNumero + segundoNumero;
+		
+		System.out.println("La suma es=" + suma);
+		
+	}
+
+}
