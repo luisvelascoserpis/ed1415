d1cd1f1220e4fbb5675a9c70eb0b2b5870b1c84a
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2014-12-10 09:13:38 +0100

    PVector VectorTest: añadidos test para el caso de vector vacío

--- PVector/test/org/institutoserpis/ed/VectorTest.java
+++ PVector/test/org/institutoserpis/ed/VectorTest.java
@@ -3,6 +3,7 @@ package org.institutoserpis.ed;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
 
 public class VectorTest {
 
@@ -13,6 +14,11 @@ public class VectorTest {
 		assertEquals(15, Vector.min(new int[] {19, 33, 16, 15, 17}));
 	}
 
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void testMinEmpty() {
+		Vector.min(new int[] {});
+	}
+	
 	@Test
 	public void testIndexOf() {
 		int[] v = new int[]{12, 33, 15, 7};
@@ -20,6 +26,13 @@ public class VectorTest {
 		assertEquals(3, Vector.indexOf(v, 7) );
 		assertEquals(1, Vector.indexOf(v, 33) );
 		assertEquals(-1, Vector.indexOf(v, 100) );
+	}
+	
+	@Test
+	public void testIndexOfEmpty() {
+		int[] v = new int[]{}; // new int[0];
+		assertEquals(-1, Vector.indexOf(v, 12));
+		
 	}
 
 }
